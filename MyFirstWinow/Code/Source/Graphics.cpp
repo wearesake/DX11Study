88b9114b0096fd434fd48c77647ecc336814867c
PanRui <2592836740@qq.com>
2025-09-05 15:47:50 +0800

    1.提交代码

--- MyFirstWinow/Code/Source/Graphics.cpp
+++ MyFirstWinow/Code/Source/Graphics.cpp
@@ -1,4 +1,4 @@
-#include "../Include/Graphics.h"
+﻿#include "../Include/Graphics.h"
 #include "../../Dxerr/dxerr.h"
 #include <sstream>
 #include <d3dcompiler.h>
@@ -81,47 +81,88 @@ void Graphics::DrawTestTriangle()
 
     struct Vertex
     {
-        float x,y;
-    };
+        struct
+        {
+            float x;
+            float y;
+        }pos;
 
+        struct
+        {
+            unsigned char r;
+            unsigned char g;
+            unsigned char b;
+            unsigned char a;
+        }color;
+    };
+    
     const Vertex vertices[] ={
-        {0.0f, 0.5f},
+        {0.5f, 0.5f},
         {0.5f, -0.5f},
         {-0.5f, -0.5f},
+
+        {0.0f, 0.0f},
+        {1.0f, 0.0f},
+        {1.0f, 1.0f},
+        
+        
     };
-    //ComPtr的&重写了，先Reset在取地址
+    
+    //输入布局（Input Layout）的描述，它告诉 D3D11 顶点缓冲区里的数据该如何解释并传递给顶点着色器。
+    //变量名 索引 数据格式 槽位 偏移 数据类型 实例化步长
+    //变量名必须和顶点着色器输入变量的语义一致
+    
+    const D3D11_INPUT_ELEMENT_DESC layout[] = {
+        
+        { "Position", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+        {"Color", 0, DXGI_FORMAT_R8G8B8A8_UNORM, 0, 8u, D3D11_INPUT_PER_VERTEX_DATA, 0}
+    };
+    
+    wrl::ComPtr<ID3DBlob>  pBlobVertex; //创建之前读入shader
+    GFX_THROW_INFO( D3DReadFileToBlob(L"./ShaderProject/VertexShader.cso", pBlobVertex.GetAddressOf() ) );
+    
+    wrl::ComPtr<ID3D11InputLayout> pInputLayout;    //ID3D11InputLayout布局类
+
+    GFX_THROW_INFO(
+        pDevice->CreateInputLayout( layout,(UINT)std::size( layout ), pBlobVertex->GetBufferPointer(), pBlobVertex->GetBufferSize(), &pInputLayout)
+    );
+
+    //（IA → VS → RS → PS → OM）
+    /* -----------------------------1.IA---------------------------------------*/
+    //设置三角形集 IA = Input Assembler 阶段 负责把顶点缓冲中的数据拼接成图元（点/线/三角形等）。
+    pDeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+    //绑定布局
+    
+    pDeviceContext->IASetInputLayout( pInputLayout.Get() );
+   
     wrl::ComPtr<ID3D11Buffer> pVertexBuffer;
     D3D11_BUFFER_DESC bufferDesc = {};
+    bufferDesc.ByteWidth = sizeof( vertices );    //缓冲区的大小（以字节为单位）。
     bufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
     bufferDesc.Usage = D3D11_USAGE_DEFAULT;
     bufferDesc.CPUAccessFlags = 0u;
     bufferDesc.MiscFlags = 0u;
-    bufferDesc.ByteWidth = sizeof( vertices );
     bufferDesc.StructureByteStride = sizeof( Vertex );
+    
     D3D11_SUBRESOURCE_DATA subresourceData = {};
-    subresourceData.pSysMem = vertices;
-    GFX_THROW_INFO( pDevice->CreateBuffer( &bufferDesc, &subresourceData, &pVertexBuffer ) );
-    const UINT stride = sizeof( Vertex );
-    const UINT offset = 0u;
+    subresourceData.pSysMem = vertices;     //pSysMem指向初始化数据的指针。
+    GFX_THROW_INFO
+    (
+        pDevice->CreateBuffer( &bufferDesc, &subresourceData, &pVertexBuffer )   //pVertexBuffer 会保存指向新建缓冲区的指针
+    );
+    const UINT stride = sizeof( Vertex );  //&stride → 指向 stride 数组的指针（告诉 GPU 每个顶点占多少字节）
+    const UINT offset = 0u;  //&offset → 指向 offset 数组的指针（告诉 GPU 从缓冲区哪个字节开始读）
+    //设置顶点缓冲区---从输入槽位0开始，绑定1个缓冲区 &stride和&offset它们都不是单个值，而是数组，每个元素对应一个缓冲区。
     pDeviceContext->IASetVertexBuffers(0u, 1u, pVertexBuffer.GetAddressOf(), &stride, &offset);
-    
-    wrl::ComPtr<ID3DBlob>  pBlob;
-    
-    //创建像素着色器
-    wrl::ComPtr<ID3D11PixelShader> pPixelShader;
-    GFX_THROW_INFO( D3DReadFileToBlob( L"./ShaderProject/PixelShader.cso", pBlob.GetAddressOf() ) );
-    GFX_THROW_INFO( pDevice->CreatePixelShader(pBlob->GetBufferPointer(), pBlob->GetBufferSize(), nullptr, &pPixelShader ) );
-    //绑定像素着色器
-    pDeviceContext->PSSetShader(pPixelShader.Get(), nullptr, 0u);
-
+    /* -----------------------------2.VS---------------------------------------*/
     //创建顶点着色器
     wrl::ComPtr<ID3D11VertexShader> pVertexShader;
-    GFX_THROW_INFO( D3DReadFileToBlob(L"./ShaderProject/VertexShader.cso", pBlob.GetAddressOf() ) );
-    GFX_THROW_INFO( pDevice->CreateVertexShader(pBlob->GetBufferPointer(), pBlob->GetBufferSize(), nullptr, &pVertexShader ) );
+    GFX_THROW_INFO( pDevice->CreateVertexShader(pBlobVertex->GetBufferPointer(), pBlobVertex->GetBufferSize(), nullptr, &pVertexShader ) );
     //绑定顶点着色器
     pDeviceContext->VSSetShader(pVertexShader.Get(), nullptr, 0u);
-
-    //配置 vireport
+    
+    /* -----------------------------3.RS---------------------------------------*/
+    //配置 viewport 设置渲染区域（裁剪空间范围）
     D3D11_VIEWPORT vp;
     vp.Width = 800;
     vp.Height = 600;
@@ -130,28 +171,19 @@ void Graphics::DrawTestTriangle()
     vp.TopLeftX = 0;
     vp.TopLeftY = 0;
     pDeviceContext->RSSetViewports(1u, &vp);
-    //输入布局
-    wrl::ComPtr<ID3D11InputLayout> pInputLayout;
-    const D3D11_INPUT_ELEMENT_DESC layout[] = {
-        { "Position", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-    };
-    GFX_THROW_INFO( pDevice->CreateInputLayout(
-        layout,(UINT)std::size( layout ),
-        pBlob->GetBufferPointer(),
-        pBlob->GetBufferSize(),
-        &pInputLayout
-    ));
-
-    //绑定布局
-    pDeviceContext->IASetInputLayout( pInputLayout.Get() );
+    /* -----------------------------4.PS---------------------------------------*/
+    wrl::ComPtr<ID3DBlob>  pBlobPixel;
+    GFX_THROW_INFO( D3DReadFileToBlob( L"./ShaderProject/PixelShader.cso", pBlobPixel.GetAddressOf() ) );
+    //创建像素着色器
+    wrl::ComPtr<ID3D11PixelShader> pPixelShader;
+    GFX_THROW_INFO( pDevice->CreatePixelShader(pBlobPixel->GetBufferPointer(), pBlobPixel->GetBufferSize(), nullptr, &pPixelShader ) );
+    //绑定像素着色器
+    pDeviceContext->PSSetShader(pPixelShader.Get(), nullptr, 0u);
     
-    //绑定render target
+    /* -----------------------------5.OM---------------------------------------*/
+    //绑定render target   渲染管线的最后一环 把像素着色器的输出结果写入 Render Target
     pDeviceContext->OMSetRenderTargets(1u, pTarget.GetAddressOf(), nullptr);
-    //&和GetAddressOf()的区别：前者在使用时会释放掉原先数据，填充调用函数中的新的数据，后者只是取得该地址
-
-    //设置三角形集
-    pDeviceContext->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-    
+    /* -----------------------------Draw---------------------------------------*/
     pDeviceContext->Draw((UINT)std::size( vertices ), 0u);
 }
 
